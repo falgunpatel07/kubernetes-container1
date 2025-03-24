9f8558f4218e3b16ab9041b954f344c036812b6f
falgunpatel07 <71875123+falgunpatel07@users.noreply.github.com>
2025-03-23 21:49:25 -0300

    latest change

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 // test
-//kube video
 // Configuration
 const PORT = process.env.PORT || 3000;
 const STORAGE_DIR = process.env.STORAGE_DIR || './storage'; // This will map to PV in k8s
